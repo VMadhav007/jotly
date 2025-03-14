f8ad96ee028f82e6bbed6f1c198c22863c3aba4d
Madhav <madhav0570@gmail.com>
2025-03-14 19:18:08 +0530

    Fixed component import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Waves from './Waves';
-import Header from "./components/Header";
+import Header from "./components/header";
 import Note from "./components/Note";
 import Input from "./components/Input";
 import "./App.css";
